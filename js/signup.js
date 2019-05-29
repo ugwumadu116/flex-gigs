3d16444ebd5a286e8eef6d3eab8f0c44139e4c08
joel ugwumadu <ugwumadu116@gmail.com>
2019-05-30 00:31:20 +0100

    adds confirm password filed to signup

--- js/signup.js
+++ js/signup.js
@@ -2,32 +2,48 @@ $("#sign-up").submit(e => {
   const firstName = $("#firstName").val();
   const lastName = $("#lastName").val();
   const password = $("#password").val();
+  const confirmPassword = $("#confirmPassword").val();
   const email = $("#email").val();
 
   let formData = JSON.stringify({ firstName, lastName, password, email });
 
-  if (!firstName.match(/^[A-Z]+$/i)) {
+  if (!firstName.match(/^[A-Z]+$/i) || !lastName.match(/^[A-Z]+$/i)) {
     Swal.fire({
       type: "error",
       title: "Oops...",
-      text: "First name should be alphabets only"
+      text: "First name and Last name should be alphabets only"
     });
-  } else if (!lastName.match(/^[A-Z]+$/i)) {
+  } else if (password !== confirmPassword) {
     Swal.fire({
       type: "error",
       title: "Oops...",
-      text: "Last name should be alphabets only"
+      text: "Password and confirm password must be the same"
     });
   } else {
     $.ajax({
-      method: "POST",
+      method: "GET",
       url: "http://localhost:3000/users",
-      dataType: "json",
-      contentType: "application/json",
-      data: formData
+      dataType: "json"
     }).done(function(data) {
-      localStorage.setItem("user", JSON.stringify(data));
-      window.location.replace("dashboard.html");
+      let user = data.find(item => item.email === email);
+      if (user) {
+        Swal.fire({
+          type: "error",
+          title: "Oops...",
+          text: `User with this email: ${email} is already registered`
+        });
+      } else {
+        $.ajax({
+          method: "POST",
+          url: "http://localhost:3000/users",
+          dataType: "json",
+          contentType: "application/json",
+          data: formData
+        }).done(function(data) {
+          localStorage.setItem("user", JSON.stringify(data.id));
+          window.location.replace("freelancers.html");
+        });
+      }
     });
   }
 
