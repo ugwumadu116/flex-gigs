f2546534838ffb33dec9c88f2cd3533cee7ecc2f
joel ugwumadu <ugwumadu116@gmail.com>
2019-05-31 03:09:20 +0100

    creates update user profile

--- js/profile.js
+++ js/profile.js
@@ -1,6 +1,7 @@
 let userInfo = JSON.parse(localStorage.getItem("user"));
 let setUser = userInfo.name.split("")[0] || "?";
 $(".firstName").text(setUser);
+let myUser = {};
 
 $(document).ready(async () => {
   let users = await $.ajax({
@@ -8,6 +9,7 @@ $(document).ready(async () => {
     url: `http://localhost:3000/users/${userInfo.id}?_embed=profiles`,
     dataType: "json"
   });
+  myUser = users;
   if (users.profiles.length <= 0) {
     $(".profile-container").html(`<div class="empty-profile">
       <h1>Please create your profile</h1>
@@ -46,6 +48,7 @@ $(document).ready(async () => {
   </div>
     `);
   }
+  await getButton();
 });
 
 $(".create").click(() => {
@@ -56,14 +59,14 @@ $(".create").click(() => {
 $("#create-profile").submit(e => {
   e.preventDefault();
   const skill = $("#skill").val();
-  const img = $("#img").val();
+  const imageUrl = $("#img").val();
   const price = $("#price").val();
   const description = $("#description").val();
 
   let formData = JSON.stringify({
     userId: userInfo.id,
     skill,
-    img,
+    imageUrl,
     price,
     description
   });
@@ -87,3 +90,52 @@ $(".close-profile").click(() => {
   $(".sell-car-overlay").toggleClass("transparentBcg2");
   $(".sell-car").toggleClass("showSell-carX");
 });
+
+//close modal 2
+$(".close-profile2").click(() => {
+  $(".sell-car-overlay2").toggleClass("transparentBcg22");
+  $(".sell-car2").toggleClass("showSell-carX2");
+});
+
+// update
+const getButton = () => {
+  $(".update").click(() => {
+    $("#skill2").val(myUser.profiles[0].skill);
+    $("#img2").val(myUser.profiles[0].imageUrl);
+    $("#price2").val(myUser.profiles[0].price);
+    $("#description2").val(myUser.profiles[0].description);
+    $(".sell-car-overlay2").toggleClass("transparentBcg22");
+    $(".sell-car2").toggleClass("showSell-carX2");
+  });
+
+  $("#create-profile2").submit(e => {
+    e.preventDefault();
+    const skill = $("#skill2").val();
+    const imageUrl = $("#img2").val();
+    const price = $("#price2").val();
+    const description = $("#description2").val();
+
+    let formData = JSON.stringify({
+      userId: userInfo.id,
+      skill,
+      imageUrl,
+      price,
+      description
+    });
+    //console.log(formData);
+    $.ajax({
+      method: "PATCH",
+      url: `http://localhost:3000/profiles/${myUser.id}`,
+      dataType: "json",
+      contentType: "application/json",
+      data: formData
+    }).done(function(data) {
+      console.log(data);
+      Swal.fire(
+        "Good job!",
+        "You have successfully updated your profile",
+        "success"
+      );
+    });
+  });
+};
